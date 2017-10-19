9ef8db2ff14bca27334c8f1741814feccdec9689
Joseph Lynch <joe.e.lynch@gmail.com>
2017-10-19 00:14:02 -0700

    Only fail replacement if we _know_ the node is up
    Previously we waited for gossip to completely converge on the target
    node before allowing a replacement, now we need that and the current
    node to believe the old node is up

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -919,7 +919,8 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
                             // update to gossip. If the node is actually alive there will be plenty of updates
                             // through heartbeats
                             long nanoDelay = (LoadBroadcaster.BROADCAST_INTERVAL / 2) * 1000000L;
-                            if (Gossiper.instance.getEndpointStateForEndpoint(existing).getUpdateTimestamp() > (System.nanoTime() - nanoDelay))
+                            EndpointState ep = Gossiper.instance.getEndpointStateForEndpoint(existing);
+                            if (ep.getUpdateTimestamp() > (System.nanoTime() - nanoDelay) && ep.isAlive())
                                 throw new UnsupportedOperationException("Cannot replace a live node... ");
                             current.add(existing);
                         }

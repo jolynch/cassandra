599d9a142aeeed162d527b82862dd1bf7b51e6f9
Dinesh A. Joshi <dinesh.joshi@apple.com>
2020-01-23 12:19:51 -0800

    Minor refactor for clarity

--- src/java/org/apache/cassandra/io/sstable/format/big/BigTableWriter.java
+++ src/java/org/apache/cassandra/io/sstable/format/big/BigTableWriter.java
@@ -83,35 +83,7 @@ public class BigTableWriter extends SSTableWriter
 
         if (compression)
         {
-            CompressionParams compressionParams = metadata().params.compression;
-            ICompressor compressor = compressionParams.getSstableCompressor();
-
-            if (lifecycleNewTracker.opType() == OperationType.FLUSH && compressor != null)
-            {
-                // When we are flushing out of the memtable throughput of the compressor is critical as flushes,
-                // especially of large tables, can queue up and potentially block writes.
-                // This optimization allows us to fall back to a faster compressor if a particular
-                // compression algorithm indicates we should. See CASSANDRA-15379 for more details.
-                switch (DatabaseDescriptor.getFlushCompression())
-                {
-                    // It is relatively easier to insert a Noop compressor than to disable compressed writing
-                    // entirely as the "compression" member field is provided outside the scope of this class.
-                    // It may make sense in the future to refactor the ownership of the compression flag so that
-                    // We can bypass the CompressedSequentialWriter in this case entirely.
-                    case none:
-                        compressionParams = CompressionParams.NOOP;
-                        break;
-                    case fast:
-                        if (!compressor.recommendedUses().contains(ICompressor.Uses.FAST_COMPRESSION))
-                        {
-                            // The default compressor is generally fast (LZ4 with 16KiB block size)
-                            compressionParams = CompressionParams.DEFAULT;
-                            break;
-                        }
-                    case table:
-                    default:
-                }
-            }
+            final CompressionParams compressionParams = compressionFor(lifecycleNewTracker.opType());
 
             dataFile = new CompressedSequentialWriter(new File(getFilename()),
                                              descriptor.filenameFor(Component.COMPRESSION_INFO),
@@ -133,6 +105,45 @@ public class BigTableWriter extends SSTableWriter
         iwriter = new IndexWriter(keyCount);
 
         columnIndexWriter = new ColumnIndex(this.header, dataFile, descriptor.version, this.observers, getRowIndexEntrySerializer().indexInfoSerializer());
+    }
+
+    /**
+     * Given an OpType, determine the correct Compression Parameters
+     * @param opType
+     * @return {@link org.apache.cassandra.schema.CompressionParams}
+     */
+    private CompressionParams compressionFor(final OperationType opType)
+    {
+        CompressionParams compressionParams = metadata().params.compression;
+        final ICompressor compressor = compressionParams.getSstableCompressor();
+
+        if (null != compressor && opType == OperationType.FLUSH)
+        {
+            // When we are flushing out of the memtable throughput of the compressor is critical as flushes,
+            // especially of large tables, can queue up and potentially block writes.
+            // This optimization allows us to fall back to a faster compressor if a particular
+            // compression algorithm indicates we should. See CASSANDRA-15379 for more details.
+            switch (DatabaseDescriptor.getFlushCompression())
+            {
+                // It is relatively easier to insert a Noop compressor than to disable compressed writing
+                // entirely as the "compression" member field is provided outside the scope of this class.
+                // It may make sense in the future to refactor the ownership of the compression flag so that
+                // We can bypass the CompressedSequentialWriter in this case entirely.
+                case none:
+                    compressionParams = CompressionParams.NOOP;
+                    break;
+                case fast:
+                    if (!compressor.recommendedUses().contains(ICompressor.Uses.FAST_COMPRESSION))
+                    {
+                        // The default compressor is generally fast (LZ4 with 16KiB block size)
+                        compressionParams = CompressionParams.DEFAULT;
+                        break;
+                    }
+                case table:
+                default:
+            }
+        }
+        return compressionParams;
     }
 
     public void mark()
